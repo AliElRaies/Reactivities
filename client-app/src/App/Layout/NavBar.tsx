b566ee8851dc2c18830ab38d764886ad705b3958
Alighandro <alirayes28@gmail.com>
2021-04-13 17:22:43 +0200

    Gooooo

--- client-app/src/App/Layout/NavBar.tsx
+++ client-app/src/App/Layout/NavBar.tsx
@@ -14,7 +14,7 @@ export default function NavBar(){
                 </Menu.Item>
                 <Menu.Item name='Activities' />
                 <Menu.Item>
-                    <Button positive content='Create Activity' />
+                    <Button onClick={() => { openForm() }} positive content='Create Activity' />
                 </Menu.Item>
             </Container>
         </Menu>
